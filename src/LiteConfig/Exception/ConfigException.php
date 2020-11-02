a7652eac44164c72a6161cdddba15a2c9a9c5ad8
Mark A <62455812+Rollylni@users.noreply.github.com>
2020-11-02 20:54:44 +0300

    Add files via upload

---
+++ src/LiteConfig/Exception/ConfigException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * LiteConfig
+ * 
+ * @copyright 2019-2020 Rollylni
+ * @author RoLLy <gdrolly@gmail.com>
+ * @version 2.0.0
+ * @license MIT
+ */
+namespace LiteConfig\Exception;
+
+use Exception;
+
+class ConfigException extends Exception {}
---
+++ src/LiteConfig/Exception/FormatException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * LiteConfig
+ * 
+ * @copyright 2019-2020 Rollylni
+ * @author RoLLy <gdrolly@gmail.com>
+ * @version 2.0.0
+ * @license MIT
+ */
+namespace LiteConfig\Exception;
+
+class FormatException extends ConfigException {}
